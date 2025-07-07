dabbafbb1efad20e576d6a23ba411041c509c47e
Bytupapa <luismaldonado6699@hotmail.com>
2025-07-06 20:10:14 -0500

    Replace C# project with C++/CLI Windows Forms skeleton

--- WorldBuilder/Entities/Ally.cs
+++
@@ -1,7 +0,0 @@
-namespace WorldBuilder.Entities
-{
-    public class Ally : Character
-    {
-        public Ally(string name) : base(name) {}
-    }
-}
--- WorldBuilder/Entities/Enemy.cs
+++
@@ -1,7 +0,0 @@
-namespace WorldBuilder.Entities
-{
-    public class Enemy : Character
-    {
-        public Enemy(string name) : base(name) {}
-    }
-}
